353381d015bcef871bcf5d99520dbe152fc1909f
Ben Ilegbodu <ben@benmvp.com>
2016-04-18 18:51:38 -0700

    Add fetch of emails w/ polling

--- final/containers/App.js
+++ final/containers/App.js
@@ -1,43 +1,46 @@
+import 'whatwg-fetch';
 import EmailForm from '../components/EmailForm';
 import EmailList from '../components/EmailList';
 import EmailView from '../components/EmailView';
 import React from 'react';
 
-const EMAILS = [
-    {
-        'id': 1,
-        'date': '01/01/2016 12:13:14',
-        'unread': false,
-        'to': 'me@abcdef.com',
-        'from': 'jane@abcdef.com',
-        'subject': 'Happy new year!',
-        'message': 'Just wanted to welcome you into 2016!'
-    },
-    {
-        'id': 3,
-        'date': '02/03/2016 15:24:58',
-        'unread': false,
-        'to': 'me@abcdef.com',
-        'from': 'potus@us.gov',
-        'subject': 'New owner of this account',
-        'message': 'In January 2017, my time in office will be over and there will be a new owner of this account'
-    },
-    {
-        'id': 2,
-        'date': '01/15/2016 19:18:17',
-        'unread': true,
-        'to': 'me@abcdef.com',
-        'from': 'kobe@bryant.com',
-        'subject': 'Retirement party!',
-        'message': '<p>Hello,</p><p>After 20 years in the NBA, I\'ve decided to hang up my shoes and call it a career. I\'ve enjoyed watching you watch me play and I would like to formally invite you to my retirement party next week. Hope you can make it!</p><p>Mamba out</p>'
-    }
-];
-
 export default class App extends React.Component {
+    static propTypes = {
+        pollInterval: React.PropTypes.number
+    }
+
+    static defaultProps = {
+        pollInterval: 2000
+    }
+
+    state = {
+        emails: []
+    }
+
+    componentDidMount() {
+        this._getUpdateEmails();
+
+        this._pollId = setInterval(
+            () => this._getUpdateEmails(),
+            this.props.pollInterval
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this._pollId);
+    }
+
+    _getUpdateEmails() {
+        fetch('/api/emails')
+            .then((res) => res.json())
+            .then((emails) => this.setState({emails}))
+            .catch((ex) => console.error(ex));
+    }
+
     render() {
         return (
             <div>
-                <EmailList emails={EMAILS} />
+                <EmailList emails={this.state.emails} />
                 <EmailView />
                 <EmailForm />
             </div>
--- final/index.js
+++ final/index.js
@@ -3,6 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-    <App />,
+    <App pollInterval={5000} />,
     document.getElementById('app')
 );
