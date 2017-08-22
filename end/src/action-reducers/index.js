75c26dac9404f2fab5fa758483c5b7689a8c3b8e
Ben Ilegbodu <ben@benmvp.com>
2017-08-21 19:25:10 -0700

    Refresh Step 12 - API lib
    - Formerly Step 13
    - Tasks: move over two API calls & factor out commonalities
    - Exercises: move over the remaining API calls

--- end/src/action-reducers/index.js
+++ end/src/action-reducers/index.js
@@ -1,58 +1,46 @@
-import * as api from '../api';
+import {addEmail, getEmails, deleteEmail, setUnread} from '../api';
 
-const setUnread = (emails, emailId, unread) => (
-    api.setUnread(emailId, unread)
-        .then(({success}) => {
-            if (success) {
-                return emails.map((email) => (
-                    email.id === emailId
-                        ? {...email, unread}
-                        : email
-                ));
-            }
+const setUnread = (emails, emailId, unread) =>
+  setUnread(emailId, unread).then(({success}) => {
+    if (success) {
+      return emails.map(
+        email => (email.id === emailId ? {...email, unread} : email)
+      );
+    }
 
-            throw new Error(`Unable to set email ID# ${emailId} unread state to ${unread}.`);
-        })
-        .catch((ex) => console.error(ex))
-);
+    throw new Error(
+      `Unable to set email ID# ${emailId} unread state to ${unread}.`
+    );
+  });
 
-export const getEmails = () => (
-    api.getEmails()
-        .catch((ex) => console.error(ex))
-);
+export const getEmails = () => getEmails();
 
 export const markRead = (emails, emailId) => setUnread(emails, emailId, false);
 
 export const markUnread = (emails, emailId) => setUnread(emails, emailId, true);
 
-export const deleteEmail = (emails, emailId) => (
-    api.deleteEmail(emailId)
-        .then(({success}) => {
-            if (success) {
-                return emails.filter((email) => email.id !== emailId);
-            }
+export const deleteEmail = (emails, emailId) =>
+  deleteEmail(emailId).then(({success}) => {
+    if (success) {
+      return emails.filter(email => email.id !== emailId);
+    }
 
-            throw new Error(`Unable to delete email ID# ${emailId}.`);
-        })
-        .catch((ex) => console.error(ex))
-);
+    throw new Error(`Unable to delete email ID# ${emailId}.`);
+  });
 
-export const addEmail = (emails, newEmail) => (
-    api.addEmail(newEmail)
-        .then(({success}) => {
-            if (success) {
-                return [
-                    {
-                        ...newEmail,
-                        id: Date.now(),
-                        date: `${new Date()}`,
-                        unread: true
-                    },
-                    ...emails
-                ];
-            }
+export const addEmail = (emails, newEmail) =>
+  addEmail(newEmail).then(({success}) => {
+    if (success) {
+      return [
+        {
+          ...newEmail,
+          id: Date.now(),
+          date: `${new Date()}`,
+          unread: true
+        },
+        ...emails
+      ];
+    }
 
-            throw new Error('Unable to send email!');
-        })
-        .catch((ex) => console.error(ex))
-);
+    throw new Error('Unable to send email!');
+  });
