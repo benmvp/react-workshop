baac1e1fae669e6a08586821bc87a1a5995e6b22
vincent-eb <vincent@eventbrite.com>
2017-09-19 22:43:43 -0700

    add modified app.js

--- 15-connect-app-and-store/src/App.js
+++ 15-connect-app-and-store/src/App.js
@@ -1,61 +1,11 @@
 import React, {PureComponent} from 'react';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
 import PropTypes from 'prop-types';
 
-import {
-  addEmail,
-  getEmails,
-  deleteEmail,
-  markRead,
-  markUnread
-} from './action-reducers';
-
-import EmailList from './components/EmailList';
-import EmailView from './components/EmailView';
-import EmailForm from './components/EmailForm';
-
-import './App.css';
-
-const EmailViewWrapper = ({
-  selectedEmail,
-  onClose,
-  onDelete,
-  onMarkUnread,
-  onMarkRead
-}) => {
-  let component = null;
-
-  if (selectedEmail) {
-    component = (
-      <article className="app__view">
-        <EmailView
-          email={selectedEmail}
-          onClose={onClose}
-          onDelete={onDelete}
-          onMarkUnread={onMarkUnread}
-          onMarkRead={onMarkRead}
-        />
-      </article>
-    );
-  }
-
-  return component;
-};
-
-const EmailFormWrapper = ({showForm, onSubmit, onCancel}) => {
-  let component = null;
-
-  if (showForm) {
-    component = (
-      <div className="app__form-modal">
-        <div className="app__form">
-          <EmailForm onSubmit={onSubmit} onCancel={onCancel} />
-        </div>
-      </div>
-    );
-  }
-
-  return component;
-};
+import {getEmails} from './actions';
+import {emails} from './reducers';
 
 export default class App extends PureComponent {
   static propTypes = {
@@ -67,19 +17,15 @@ export default class App extends PureComponent {
     pollInterval: 2000
   };
 
-  state = {
-    // Initialize emails state to an empty array.
-    // Will get populated with data in `componentDidMount`
-    emails: [],
-    // Initialize selected email ID to -1, indicating nothing is selected.
-    // When an email is selected in EmailList, this will be updated to
-    // corresponding ID
-    selectedEmailId: -1,
-    // Initialize show form flag to false, indicating that it won't show.
-    // When the new email button is clicked, it'll be set to `true`. It'll
-    // be toggled false on form submission or cancel
-    showForm: false
-  };
+
+  constructor(props) {
+    super(props);
+
+    this._store = createStore(
+      emails,
+      applyMiddleware(thunk)
+    );
+  }
 
   componentDidMount() {
     // Retrieve emails from server once we know DOM exists
@@ -100,103 +46,16 @@ export default class App extends PureComponent {
   }
 
   _getUpdateEmails() {
-    return getEmails().then(emails => this.setState({emails}));
-  }
-
-  _handleItemSelect(selectedEmailId) {
-    // update state (so that the EmailView will show)
-    this.setState({selectedEmailId});
-
-    if (this.state.selectedEmailId !== selectedEmailId) {
-      // also mark the email as read
-      this._handleItemMarkRead(selectedEmailId);
-    }
-  }
-
-  _handleEmailViewClose() {
-    // We close the email view by resetting the selected email
-    this.setState({selectedEmailId: -1});
-  }
-
-  _handleFormSubmit(newEmail) {
-    addEmail(this.state.emails, newEmail)
-      // if the email was successfully updated, we have to make
-      // a request to get the new list of emails, but we'll have
-      // to wait for the response of that request, so let's add to
-      // our state immediately and then later when the response
-      // comes back, the server-side list will update. This is mainly
-      // here to demonstrate immutable updating of data structures
-      .then(emails => this.setState({emails, showForm: false}));
-  }
-
-  _handleItemDelete(emailId) {
-    deleteEmail(this.state.emails, emailId)
-      // optimistic updating (see _handleFormSubmit for more info)
-      // Also reset `selectedEmailId` since we're deleting it
-      .then(emails => this.setState({emails, selectedEmailId: -1}));
-  }
-
-  _handleItemMarkUnread(emailId) {
-    markUnread(this.state.emails, emailId)
-      // optimistic updating (see _handleFormSubmit for more info)
-      .then(emails => this.setState({emails}));
-  }
-
-  _handleItemMarkRead(emailId) {
-    markRead(this.state.emails, emailId)
-      // optimistic updating (see _handleFormSubmit for more info)
-      .then(emails => this.setState({emails}));
-  }
-
-  _handleShowForm() {
-    // Show email form overlay by setting state to true
-    this.setState({showForm: true});
-  }
-
-  _handleHideForm() {
-    // Hide email form overlay by setting state to false
-    this.setState({showForm: false});
+    getEmails();
   }
 
   render() {
-    let {emails, selectedEmailId, showForm} = this.state;
-    let selectedEmail = emails.find(email => email.id === selectedEmailId);
+    let {pollInterval} = this.props;
 
     return (
-      <main className="app">
-        <div className="app__page">
-          <div className="app__list">
-            <EmailList
-              emails={emails}
-              onItemSelect={this._handleItemSelect.bind(this)}
-              onItemDelete={this._handleItemDelete.bind(this)}
-              onItemMarkUnread={this._handleItemMarkUnread.bind(this)}
-              selectedEmailId={selectedEmailId}
-            />
-          </div>
-          <EmailViewWrapper
-            selectedEmail={selectedEmail}
-            onClose={this._handleEmailViewClose.bind(this)}
-            onDelete={this._handleItemDelete.bind(this, selectedEmailId)}
-            onMarkUnread={this._handleItemMarkUnread.bind(
-              this,
-              selectedEmailId
-            )}
-            onMarkRead={this._handleItemMarkRead.bind(this, selectedEmailId)}
-          />
-          <button
-            className="app__new-email"
-            onClick={this._handleShowForm.bind(this)}
-          >
-            +
-          </button>
-          <EmailFormWrapper
-            showForm={showForm}
-            onSubmit={this._handleFormSubmit.bind(this)}
-            onCancel={this._handleHideForm.bind(this)}
-          />
-        </div>
-      </main>
+      <Provider store={this._store}>
+        <Page pollInterval={pollInterval} />
+      </Provider>
     );
   }
 }
