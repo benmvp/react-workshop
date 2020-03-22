ccd8d2d7ce9a33901bd3489cbd2a0719cd4f669d
Ben Ilegbodu <ben@benmvp.com>
2020-03-22 14:31:31 -0700

    Update dependencies & CI

--- src/setupTests.js
+++
@@ -1,9 +0,0 @@
-import {configure} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-// setups up jest + enzyme matchers (https://github.com/FormidableLabs/enzyme-matchers)
-import 'jest-enzyme';
-
-configure({adapter: new Adapter()});
-
-// TODO: Cause test failures for console.warn/error
